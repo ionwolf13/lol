eb46ddf6fa2b95abc5370b07dc9b0cfb15d3d871
jesus_garcia <voli.scorp13@gmail.com>
2021-07-01 21:26:45 -0500

    added

---
+++ src/components/presentational/HomeComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const HomeComponent = () => {
+
+    // function getCharacters(){
+    //     fetch('http://ddragon.leagueoflegends.com/cdn/11.13.1/data/en_US/champion.json')
+    //     .then(res => JSON.parse(res))
+    //     .then(res => console.log(res))
+    // }
+
+    // getCharacters();
+    return(
+        <div>
+            <p>Home HomeComponent</p>
+        </div>
+    )
+}
